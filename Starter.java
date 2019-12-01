fdcf8e8b3d4f4aee71bd2dc5ee8b8b1393705c82
gerardolq <58379792+gerardolq@users.noreply.github.com>
2019-12-01 13:03:50 -0800

    Add files via upload

---
+++ Starter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import CFCode.CFPlayer;
+import CFCode.RandomAI;
+import CFCode.SmartAI;
+import CFCode.ConsoleCF;
+import CFCode.GUICF;
+import CFCode.StartGame;
+
+
+public class Starter{
+
+  public static void main(String[] args){
+  	new StartGame();
+  }
+}
